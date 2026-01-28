99257f74a5272bbb3518b51f89754f33901e4f2b
Rafael Rosa <rafa.rafaelrs97@gmail.com>
2026-01-28 10:46:10 -0300

    bump app version to 0.3.2

--- pages/Profile.tsx
+++ pages/Profile.tsx
@@ -285,7 +285,7 @@ const Profile = () => {
             </div>
 
             <div className="text-center pb-8 pt-4">
-                <p className="text-[10px] text-gray-700 font-mono">DOPAMIND v0.3.1 • Built with Aura</p>
+                <p className="text-[10px] text-gray-700 font-mono">DOPAMIND v0.3.2 • Built with Aura</p>
             </div>
 
             <ProModal isOpen={isProModalOpen} onClose={() => setIsProModalOpen(false)} />
